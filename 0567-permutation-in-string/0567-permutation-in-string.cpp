1a591273741fe04501c40557810c1c7348fa6cae
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-06-30 05:08:10 +0530

    Time: 11 ms (30.63%), Space: 11 MB (18.18%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int> mp1, mp2;
+
+for(char c : s1)
+    mp1[c]++;
+
+int left = 0;
+
+for(int right = 0; right < s2.size(); right++){
+
+    // add
+    mp2[s2[right]]++;
+
+
+    if(right - left + 1 > s1.size()){
+
+        mp2[s2[left]]--;
+
+        if(mp2[s2[left]] == 0)
+            mp2.erase(s2[left]);
+
+        left++;
+    }
+
+    if(right - left + 1 == s1.size()){
+
+        if(mp1 == mp2)
+            return true;
+    }
+}
+
+return false;
+        
+    }
+};
