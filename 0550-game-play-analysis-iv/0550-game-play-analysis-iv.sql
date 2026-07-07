12b357dc1d980bd8b4b01e3c260454cd873a130d
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-07-08 00:45:01 +0530

    Time: 708 ms (17.82%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -2,14 +2,9 @@
 #fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places.
 #this is self join so we can figure out first login and second login
 SELECT
-ROUND(
-    COUNT(*) /
-    (SELECT COUNT(DISTINCT player_id) FROM Activity),2) AS fraction
-FROM(
-    SELECT player_id, MIN(event_date) AS first_login
-    FROM Activity
-    GROUP BY player_id
-) f
+ROUND(COUNT(*) /(SELECT COUNT(DISTINCT player_id) FROM Activity),2) AS fraction
+FROM(SELECT player_id, MIN(event_date) AS first_login FROM Activity
+GROUP BY player_id) f
 JOIN Activity a
 ON f.player_id = a.player_id
 AND a.event_date = DATE_ADD(f.first_login, INTERVAL 1 DAY);
