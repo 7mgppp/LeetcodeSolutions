f0000ed50c19b1625f12ac47682a43ea65254019
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-07-13 01:33:39 +0530

    Time: 4 ms (82.63%), Space: 26.1 MB (80.67%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -4,35 +4,32 @@ public:
 
         int maxi = INT_MIN;
         int ans = 0;
-        for(int i =0; i<nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++) {
             maxi = max(maxi, nums[i]);
         }
 
         int low = 1;
         int high = maxi;
 
-        while(low <= high){
-            int mid = (low + high)/2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
             int sum = 0;
 
-            for(int i = 0; i<nums.size(); i++){
-                int addend = ceil((double)nums[i]/mid);
-                sum +=  addend;
+            for (int i = 0; i < nums.size(); i++) {
+                int addend = ceil((double)nums[i] / mid);
+                sum += addend;
             }
 
-            if(sum <= threshold){
+            if (sum <= threshold) {
                 ans = mid;
                 high = mid - 1;
             }
 
-            else{
-                low = mid +1;
+            else {
+                low = mid + 1;
             }
         }
 
-
         return ans;
-
-        
     }
 };
