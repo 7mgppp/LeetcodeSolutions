6f6047e7b8717f0b8fa0476e090fb3e4391723af
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-07-23 00:51:16 +0530

    Time: 24 ms (77.95%), Space: 23.2 MB (31.38%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+
+                if (grid[i][j] == '1') {
+                    count++;
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    grid[i][j] = '0';
+
+                    while (!q.empty()) {
+                        pair<int, int> node = q.front();
+                        q.pop();
+
+                        int r = node.first;
+                        int c = node.second;
+
+                        int dr[] = {1, -1, 0, 0};
+                        int dc[] = {0, 0, -1, 1};
+
+                        for (int k = 0; k < 4; k++) {
+                            int newRow = r + dr[k];
+                            int newCol = c + dc[k];
+
+                            if (newRow >= 0 && newRow < grid.size() &&
+                                newCol >= 0 && newCol < grid[0].size() &&
+                                grid[newRow][newCol] == '1') {
+                                grid[newRow][newCol] = '0';
+                                q.push({newRow, newCol});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+};
