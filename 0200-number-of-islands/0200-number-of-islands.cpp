52a7877629fb2c3b2580e65122dc0a42659674e9
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-08-01 23:08:19 +0530

    Time: 29 ms (41.31%), Space: 16.2 MB (80.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,42 +1,39 @@
 class Solution {
+private:
+    void dfs(int i, int j, vector<vector<char>>& grid) {
+        // define the base case
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) {
+            return;
+        }
+        if (grid[i][j] == '0') {
+            return;
+        }
+
+        // mark visited
+        grid[i][j] = '0';
+
+        // visit the neighbours
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, 1, 0, -1};
+
+        for (int k = 0; k < 4; k++) {
+            dfs(i + dr[k], j + dc[k], grid);
+        }
+    }
+
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int count = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[i].size(); j++) {
 
+        int count = 0;
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == '1') {
                     count++;
-                    queue<pair<int, int>> q;
-                    q.push({i, j});
-                    grid[i][j] = '0';
-
-                    while (!q.empty()) {
-                        pair<int, int> node = q.front();
-                        q.pop();
-
-                        int r = node.first;
-                        int c = node.second;
-
-                        int dr[] = {1, -1, 0, 0};
-                        int dc[] = {0, 0, -1, 1};
-
-                        for (int k = 0; k < 4; k++) {
-                            int newRow = r + dr[k];
-                            int newCol = c + dc[k];
-
-                            if (newRow >= 0 && newRow < grid.size() &&
-                                newCol >= 0 && newCol < grid[0].size() &&
-                                grid[newRow][newCol] == '1') {
-                                grid[newRow][newCol] = '0';
-                                q.push({newRow, newCol});
-                            }
-                        }
-                    }
+                    dfs(i, j, grid);
                 }
             }
         }
-
         return count;
     }
 };
