f47fdf7e15dd017839a438accf394d3dd43826ee
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-08-02 00:44:55 +0530

    Time: 0 ms (100%), Space: 27.4 MB (79.81%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,45 +1,43 @@
 class Solution {
+private:
+    int dfs(int i, int j, vector<vector<int>>& grid) {
+
+        int area = 1;
+        // base condition
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) {
+            return 0;
+        }
+
+        if (grid[i][j] == 0) {
+            return 0;
+        }
+
+        // mark visited
+        grid[i][j] = 0;
+
+        // vist neighbours
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        for (int k = 0; k < 4; k++) {
+            area += dfs(i + dr[k], j + dc[k], grid);
+        }
+
+        return area;
+    }
+
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int count = 0;
         int maxi = 0;
-        for(int i =0; i<grid.size(); i++){
-            for(int j = 0; j<grid[i].size(); j++){
-                if(grid[i][j] == 1){
-                    count = 1;
-                    queue<pair<int,int>>q;
-                    q.push({i,j});
-                    grid[i][j] = 0;
 
-                    while(!q.empty()){
-                        pair<int, int> node = q.front();
-                        q.pop();
-                        int r = node.first;
-                        int c = node.second;
-
-                        int dr[] = {1, -1, 0, 0};
-                        int dc[] = {0, 0, -1, 1};
-
-                        for(int k = 0; k<4; k++){
-                    
-                            int newRow = r + dr[k];
-                            int newCol = c + dc[k];
-
-                            if(newRow >= 0 && newRow < grid.size() && newCol >= 0 && newCol < grid[0].size() && grid[newRow][newCol] == 1){
-                                q.push({newRow,newCol});
-                                grid[newRow][newCol] = 0;
-                                count++;
-                            }
-                        }
-                    }
-                    maxi = max(count, maxi);
-
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 1) {
+                    maxi = max(maxi, dfs(i, j, grid));
                 }
-
             }
         }
-        
+
         return maxi;
-        
     }
 };
