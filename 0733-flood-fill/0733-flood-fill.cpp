791efb1fc1df43d9798c49a0526d57dc90b71fc6
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-08-01 23:54:13 +0530

    Time: 0 ms (100%), Space: 18.1 MB (75.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,40 +1,40 @@
 class Solution {
+private:
+    void dfs(int i, int j, vector<vector<int>>& image,int originalColor,
+         int newColor){
+        //base case
+        if(i < 0 || j< 0 || i >= image.size() || j >= image[0].size()){
+            return;
+        }
+
+        if(image[i][j] != originalColor){
+            return;
+        }
+
+        //visted
+        image[i][j] = newColor;
+
+        //visit neighbours
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        for(int k =0; k<4; k++){
+            dfs(i + dr[k], j + dc[k], image, originalColor,
+    newColor);
+        }
+
+    }
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int color) {
-
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int originalColor = image[sr][sc];
-
-        if (originalColor == color) {
+        if (originalColor == color)
             return image;
-        }
-
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
-
-        image[sr][sc] = color;
-
-        while (!q.empty()) {
-            pair<int, int> node = q.front();
-            q.pop();
-
-            int r = node.first;
-            int c = node.second;
-
-            int dr[] = {-1, 1, 0, 0};
-            int dc[] = {0, 0, -1, 1};
-
-            for (int i = 0; i < 4; i++) {
-                int NewRow = r + dr[i];
-                int NewCol = c + dc[i];
-
-                if (NewRow >= 0 && NewRow < image.size() && NewCol >= 0 && NewCol < image[0].size() &&image[NewRow][NewCol] == originalColor) {
-                    image[NewRow][NewCol] = color;
-                    q.push({NewRow, NewCol});
-                }
-            }
-        }
+        
+        dfs(sr, sc, image, originalColor, color);
 
         return image;
+
     }
+
+        
 };
