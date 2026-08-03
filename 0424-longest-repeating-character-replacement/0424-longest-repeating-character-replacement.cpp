55b2be0f60a1f83d8134bb4b8769062d06dfde40
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-08-04 01:09:35 +0530

    Time: 19 ms (12.75%), Space: 10.9 MB (71.49%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char,int>mp;
-        int max_freq = 0;
-        int main_maxi = 0;
         int left = 0;
+        int maxi = 0;
+        int maxFreq = 0;
+        unordered_map<int,int>mp;
 
-        //valid condition
         for(int i =0; i<s.size(); i++){
             mp[s[i]]++;
-            max_freq = max(max_freq, mp[s[i]]);
-            
-            //length of string till now - max occured char > k that means no more replacements can be made
-            while(i-left+1 - max_freq > k){
+            maxFreq = max(maxFreq, mp[s[i]]);
+
+            while(i-left+1 - maxFreq > k){  // invalid condition
                 mp[s[left]]--;
                 left++;
-            } 
+            }
 
-            main_maxi = max(main_maxi, i-left+1);
-
+            maxi = max(maxi, i-left+1);
         }
 
-        return main_maxi;
-
+        return maxi;
         
     }
 };
