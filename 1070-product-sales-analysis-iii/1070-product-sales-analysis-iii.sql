9732f8ee5ca85dbbabdacfd0f06437ffc53efe72
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-08-09 21:06:16 +0530

    Time: 761 ms (48.57%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,11 +1,13 @@
 # Write your MySQL query statement below
-#find all sales that occurred in the first year each product was sold.
-SELECT s.product_id, s.year AS first_year, s.quantity,s.price
-FROM Sales s
-JOIN(
-SELECT product_id, MIN(year) AS first_year, quantity, price 
-FROM Sales
-GROUP BY product_id
-) f
-ON s.product_id = f.product_id
-AND s.year = f.first_year;
+WITH helperTable AS(
+    SELECT product_id, 
+    year AS first_year,
+    quantity,
+    price,
+    DENSE_RANK()
+    OVER(PARTITION BY product_id ORDER BY year ASC) AS rnk
+    FROM Sales
+)
+
+SELECT product_id, first_year, quantity, price FROM helperTable
+WHERE rnk = 1;
