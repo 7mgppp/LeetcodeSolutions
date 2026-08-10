46826537c14b727def9e4ff0b17d881e24b88858
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-08-11 03:33:04 +0530

    Time: 2 ms (57.23%), Space: 27.4 MB (79.06%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,26 +1,22 @@
 class Solution {
-private:
+    
     int dfs(int i, int j, vector<vector<int>>& grid) {
-
         int area = 1;
         // base condition
-        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) {
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) {
             return 0;
         }
-
         if (grid[i][j] == 0) {
             return 0;
         }
 
-        // mark visited
+        // visied
         grid[i][j] = 0;
 
-        // vist neighbours
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
-
+        int dr[] = {0, 0, -1, 1};
+        int dc[] = {-1, 1, 0, 0};
         for (int k = 0; k < 4; k++) {
-            area += dfs(i + dr[k], j + dc[k], grid);
+            area+= dfs(i + dr[k], j + dc[k], grid);
         }
 
         return area;
@@ -29,15 +25,16 @@ private:
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int maxi = 0;
-
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == 1) {
-                    maxi = max(maxi, dfs(i, j, grid));
+        for(int i =0; i<grid.size(); i++){
+            for(int j = 0; j<grid[0].size(); j++){
+                if(grid[i][j] == 1){
+                   int currArea = dfs(i, j, grid);
+                    maxi = max(maxi, currArea);
                 }
             }
         }
 
         return maxi;
+
     }
 };
