f83016cd0679ef551694226b529184e366f3fc69
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-08-14 20:02:31 +0530

    Time: 3 ms (75.31%), Space: 19.2 MB (68.08%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,51 +1,43 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int count = 0;
-
-        //adjency list
         vector<vector<int>> adj(numCourses);
-        for (auto& it : prerequisites) {
-            adj[it[1]].push_back(it[0]);
+        for (int i = 0; i < prerequisites.size(); i++) {
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
 
-        //count indegree
         vector<int> indegree(numCourses, 0);
         for (int i = 0; i < numCourses; i++) {
-            for (int neigh : adj[i]) {
+            for (int j = 0; j < adj[i].size(); j++) {
+                int neigh = adj[i][j];
                 indegree[neigh]++;
             }
         }
 
-
         queue<int> q;
 
-        //push all the nodes with indegree = 0 in queue
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0)
+            if (indegree[i] == 0) {
                 q.push(i);
+            }
         }
 
-        //bfs
-        while(!q.empty()){
+        int count = 0;
+
+        while (!q.empty()) {
             int node = q.front();
             q.pop();
-
-            
-            for(int i = 0; i < adj[node].size(); i++){
+            for (int i = 0; i < adj[node].size(); i++) {
                 int neigh = adj[node][i];
 
                 indegree[neigh]--;
-                if(indegree[neigh] == 0){
+                if (indegree[neigh] == 0)
                     q.push(neigh);
-                }
-                
             }
-            count++;
 
+            count++;
         }
 
-        return numCourses == count;
-
+        return count == numCourses;
     }
 };
