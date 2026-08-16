0b5b0c255eec2649c244902f8aebd44c52bd128d
Miilee <MIILEE.23fe10cds00166@muj.manipal.edu>
2026-08-17 01:20:52 +0530

    Time: 0 ms (100%), Space: 10.7 MB (47.19%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    int solve(int i, vector<int>& nums, vector<int>&dp){
+        
+        if(nums.size() <= i){
+            return 0;
+        }
+
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        return dp[i] = max(nums[i]+solve(i+2, nums, dp), solve(i+1, nums, dp));
+    }
+public:
+    int rob(vector<int>& nums) {
+
+        vector<int>dp(nums.size(), -1);
+        return solve(0, nums, dp);
+        
+    }
+};
